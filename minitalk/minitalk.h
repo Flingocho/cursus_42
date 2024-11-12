5b5c5b757ccb0c5ed8f3fda0a815eccd456ceaee
Jaime Vidal Tejada <jvidal-t@c1r6s2.42madrid.com>
2024-11-12 18:11:41 +0100

    minitalk

---
+++ minitalk/minitalk.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvidal-t <jvidal-t@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 11:52:12 by jvidal-t          #+#    #+#             */
+/*   Updated: 2024/11/12 17:05:46 by jvidal-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+# include "libft.h"
+# include <limits.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# define MAX_BITS 31
+# define WAIT_TIME 10
+
+typedef struct s_mini
+{
+	int					pid;
+	char				*str_to_send;
+	int					len;
+}						t_mini;
+
+
+#endif
